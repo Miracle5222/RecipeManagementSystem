cb7c6950fc7332885b7814bed56a41d1851d0b8e
Miracle5222 <roneilgungobbansas5222@gmail.com>
2023-05-18 23:28:16 -0800

    recipe

--- client/pages/recipe.php
+++ client/pages/recipe.php
@@ -557,69 +557,84 @@ $_SESSION['mainIngridients'] = $arr1;
                         </div>
                     </div>
                 </div>
-                <?php
 
-                $sql = "SELECT user_tbl.`username`, comment_tbl.`comment`, comment_tbl.`comment_date`, comment_tbl.`ratings`, comment_tbl.`recipe_id`,comment_tbl.`comment_date`   FROM comment_tbl INNER JOIN user_tbl ON user_tbl.`user_id` = comment_tbl.`user_id` where comment_tbl.recipe_id = '$_GET[id]' group by comment_tbl.comment_id limit 2";
+                <?php
+                $sql = "SELECT user_tbl.username, comment_tbl.comment, comment_tbl.comment_date, comment_tbl.ratings, comment_tbl.recipe_id, comment_tbl.comment_date 
+        FROM comment_tbl 
+        INNER JOIN user_tbl ON user_tbl.user_id = comment_tbl.user_id 
+        WHERE comment_tbl.recipe_id = '$_GET[id]' 
+        GROUP BY comment_tbl.comment_id 
+        LIMIT 2";
                 $result = $conn->query($sql);
 
                 if ($result->num_rows > 0) {
-                    // output data of each row
+                    // Output data of each row
                     while ($row = $result->fetch_assoc()) {
-
                 ?>
-                        <div id="txtHint">
+                        <div>
                             <div style="width: 150px;" class="align-items-center justify-content-center mt-2">
-                                <div style="height: 40px; width: 40px ; margin-left:40px;" class="d-flex justify-content-center align-items-center">
+                                <div style="height: 40px; width: 40px; margin-left: 40px;" class="d-flex justify-content-center align-items-center">
                                     <img src="../assets/images/user.png" width="100%" height="100%" alt="profile-user">
                                     <span class="pl-2"><?= $row['username'] ?></span>
-
-
                                 </div>
-
                             </div>
-                            <div class=" w-100 mt-2">
-
+                            <div class="w-100 mt-2">
                                 <div style="margin-left: 80px;">
                                     <div class="d-flex justify-content-between">
                                         <h6 class="m-0"><?= $row['comment'] ?></h6>
-
-                                        <div class="d-flex " style="float: right;">
+                                        <div class="d-flex" style="float: right;">
                                             <?php
                                             if ($row['ratings'] != 0) {
-
                                                 for ($ratings = 1; $ratings <= $row['ratings']; $ratings++) {
-
-
                                             ?>
-                                                    <div style=" height: 15px; " class="mx-2 ">
-                                                        <img class=" d-block image " src=" ../../server/uploads/images/star.png " alt=" ratings">
-
+                                                    <div style="height: 15px;" class="mx-2">
+                                                        <img class="d-block image" src="../../server/uploads/images/star.png" alt="ratings">
                                                     </div>
-                                                <?php     } ?>
+                                                <?php
+                                                }
+                                                ?>
                                                 <span class="ml-4"><?= $row['comment_date'] ?></span>
-                                            <?php } ?>
+                                            <?php
+                                            }
+                                            ?>
                                         </div>
-
-
-
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     <?php
-
                     }
-                } else { ?>
+                } else {
+                    ?>
                     <div class="bg-light my-4 p-4">
                         <h3 class="lead">No comments available</h3>
                     </div>
-                <?php }
+                <?php
+                }
                 ?>
-                <div class="d-flex justify-content-center mt-4">
-                    <button type="button" class="btn btn-primary" onclick="showHint()" id="show-more-button">Show More</button>
 
+               
+                <div class="d-flex justify-content-center mt-4">
+                    <button type="button" class="btn btn-primary" onclick="showMoreComments()" id="show-more-button">Show More</button>
                 </div>
+
+                <script>
+                    var offset = 2; // Number of comments already shown
+
+                    function showMoreComments() {
+                        var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                                var response = this.responseText;
+                                document.getElementById("comments-container").innerHTML += response;
+                                offset += 3; // Increase the offset by 3 for the next set of comments
+                            }
+                        };
+                        xhttp.open("GET", "recipe.php?id=<?php echo $_GET['id']; ?>&offset=" + offset, true);
+                        xhttp.send();
+                    }
+                </script>
+
             </div>
             <div class="col-md-2">
 
